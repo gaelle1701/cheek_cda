78a5ad75acf6bc95a6fd6a56a6923199ba18677c
gaelle1701 <gaellecoue@gmail.com>
2022-09-09 19:36:13 +0200

    feat: add business logics modules

--- cheek-app/src/app/core/components/header/header.component.ts
+++ cheek-app/src/app/core/components/header/header.component.ts
@@ -1,15 +1,58 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+        height: '100vh',
+        opacity: 1,
+        backgroundColor: 'white',
+        zIndex: 1
+      })),
+  
+      state('closed', style({
+        height: '0px',
+        visibility: 'hidden',
+      })),
+  
+      transition('open => closed', [
+        animate('0.2s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ]
 })
+
+
+
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isOpen: boolean = false;
+ 
+  constructor (private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  
+  toggle= () =>{
+    this.isOpen = !this.isOpen;
+  }
+
+  toggleHome= () => {
+    this.isOpen = false
+  }
+
+  logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('role')
+    this.router.navigate(['/']);
   }
 
 }
--- cheek-app/src/app/products/products.module.ts
+++ cheek-app/src/app/products/products.module.ts
@@ -2,25 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
-import { PageAddProductComponent } from './pages/page-add-product/page-add-product.component';
-import { PageEditProductComponent } from './pages/page-edit-product/page-edit-product.component';
-import { PageListProductComponent } from './pages/page-list-product/page-list-product.component';
+
 
 
 @NgModule({
   declarations: [
-    PageAddProductComponent,
-    PageEditProductComponent,
-    PageListProductComponent
+
   ],
   imports: [
     CommonModule,
     ProductsRoutingModule
   ],
-  exports: [
-    PageAddProductComponent,
-    PageEditProductComponent,
-    PageListProductComponent
-  ]
+  // exports: [
+  //   ProductsListComponent,
+  //   ManageProductsComponent
+  // ]
 })
 export class ProductsModule { }
