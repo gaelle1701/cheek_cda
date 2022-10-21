205ecdfd13af5cf6051afe42bcb1bc5740576350
gaelle1701 <gaellecoue@gmail.com>
2022-10-21 17:21:26 +0200

    add install jest

---
+++ cheek-api/src/test/auth.test.ts
@@ -0,0 +1,7 @@
+describe('AuthController', () => {
+    it("should authenticated when login", async() => {
+        
+    })
+
+    it('should create a user when it call registered', async() => {})   
+})
